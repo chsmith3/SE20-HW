16b2543570eaa3bcd7be8bfc8276c12737a66cd5
chsmith3 <chsmith3@ncsu.edu>
2020-08-14 04:55:45 -0400

    more testing things

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='hmkachha@ncsu.edu',
     url='https://github.com/chsmith/SE20-HW',
 
-    packages=find_packages(include=['SE20HW', 'SE20HW.*', 'data']),
+    packages=find_packages(include=['SE20-HW', 'SE20-HW.*', 'data']),
     long_description="""demonstrating tools for a good repo""",
     classifiers=[
         "License :: MIT License",
